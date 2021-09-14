6233ee30e8b5a31357d3fdd6cb5d188426c7a137
ana-nikolaeva <68301710+ana-nikolaeva@users.noreply.github.com>
2021-09-15 00:49:44 +0300

    Add files via upload

---
+++ Python_Nikolaeva/Ex_1.py
@@ -0,0 +1,12 @@
+print('Спорим, что введешь любое число, а моя программа сделает из него цифру 4? Попробуем?')
+test_number1 = input('Крути барабан, сынок: ')
+test_number1 = int(test_number1)
+print('Умножаю твое число на 2, получаем: ', test_number1 * 2)
+test_number2 = int(test_number1 * 2)
+print('А теперь прибавим 8: ', test_number2 + 8)
+test_number3 = int(test_number2 + 8)
+print('Делим на 2: ', test_number3 // 2)
+test_number4 = int(test_number3 // 2)
+print('И последнее... Отнимаем твое число и получается...: ', test_number4 - test_number1)
+
+
